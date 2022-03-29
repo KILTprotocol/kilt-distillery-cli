7f94b575b35e5e6b9427801abd364226638e7e19
dudleyneedham <dudleyneedham1993@gmail.com>
2022-03-29 11:57:09 +0200

    feat: separating the util functions and work on setup verifier to work with 26

--- menus/setup-claimer.js
+++ menus/setup-claimer.js
@@ -1,345 +1,21 @@
-import {
-  Claim,
-  Attestation,
-  RequestForAttestation,
-  CType,
-  Did,
-  Balance,
-  init,
-  Utils,
-  KeyRelationship,
-  BlockchainUtils,
-  ChainHelpers,
-  VerificationKeyType,
-  EncryptionKeyType,
-} from '@kiltprotocol/sdk-js'
-import {
-  naclBoxPairFromSecret,
-  sr25519PairFromSeed,
-  mnemonicToMiniSecret,
-  keyExtractPath,
-  keyFromPath,
-  blake2AsU8a,
-  encodeAddress,
-} from '@polkadot/util-crypto'
+import { Claim, init, ChainHelpers } from '@kiltprotocol/sdk-js'
 import { getMnemonic, status, mainMenu, getClaimDetails } from './_prompts.js'
+import {
+  getAllSocialCTypes,
+  attestClaim,
+  loadAccount,
+  getKeypairs,
+  getDidDoc,
+} from './utils/utils.js'
 
 import chalk from 'chalk'
 import fs from 'fs'
-
-const resolveOn = BlockchainUtils.IS_FINALIZED
 
 async function connect() {
   await status('connecting to network...')
   await init({ address: 'wss://peregrine.kilt.io/parachain-public-ws' })
 }
 
-async function loadAccount(seed) {
-  await status('loading account...')
-  const signingKeyPairType = 'sr25519'
-  const keyring = new Utils.Keyring({
-    ss58Format: 38,
-    type: signingKeyPairType,
-  })
-  const account = keyring.addFromMnemonic(seed)
-  return account
-}
-
-async function getKeypairs(account, mnemonic) {
-  await status('generating keypairs...')
-  const keypairs = {
-    authentication: account.derive('//did//0'),
-    assertion: account.derive('//did//assertion//0'),
-    keyAgreement: (function () {
-      const secretKeyPair = sr25519PairFromSeed(mnemonicToMiniSecret(mnemonic))
-      const { path } = keyExtractPath('//did//keyAgreement//0')
-      const { secretKey } = keyFromPath(secretKeyPair, path, 'sr25519')
-      const blake = blake2AsU8a(secretKey)
-      const boxPair = naclBoxPairFromSecret(blake)
-      return {
-        ...boxPair,
-        type: 'x25519',
-      }
-    })(),
-  }
-
-  return {
-    ...keypairs,
-    relationships: {
-      [KeyRelationship.authentication]: keypairs.authentication,
-      [KeyRelationship.assertionMethod]: keypairs.assertion,
-      [KeyRelationship.keyAgreement]: keypairs.keyAgreement,
-    },
-  }
-}
-
-async function getDidDoc(account, keypairs) {
-  await status('checking for existing DID...')
-  const { api } =
-    await ChainHelpers.BlockchainApiConnection.getConnectionOrConnect()
-  const identifier = Did.DidUtils.getKiltDidFromIdentifier(
-    encodeAddress(keypairs.authentication.publicKey, 38),
-    'full'
-  )
-
-  let didDoc = await Did.DidResolver.resolveDoc(identifier)
-  if (didDoc) {
-    await status('DID loaded from chain...')
-    return didDoc
-  }
-
-  await status('checking balance...')
-
-  let balance = parseInt(
-    (await Balance.getBalances(account.address)).free.toString()
-  )
-  if (balance < 3) {
-    const message = chalk.red(
-      `get testnet tokens to continue => https://faucet.peregrine.kilt.io/?${account.address}`
-    )
-
-    await status(message)
-    while (balance < 3) {
-      balance = parseInt(
-        (await Balance.getBalances(account.address)).free.toString()
-      )
-      await new Promise((r) => setTimeout(r, 2000))
-    }
-  }
-
-  await new Did.FullDidCreationBuilder(api, {
-    publicKey: keypairs.authentication.publicKey,
-    type: VerificationKeyType.Sr25519,
-  })
-    .addEncryptionKey({
-      publicKey: keypairs.keyAgreement.publicKey,
-      type: EncryptionKeyType.X25519,
-    })
-    .setAttestationKey({
-      publicKey: keypairs.assertion.publicKey,
-      type: VerificationKeyType.Sr25519,
-    })
-    .consumeWithHandler(
-      {
-        async sign({ data, alg }) {
-          const { authentication } = keypairs
-          return {
-            data: authentication.sign(data, { withType: false }),
-            alg,
-          }
-        },
-      },
-      account.address,
-      async (creationTx) => {
-        await BlockchainUtils.signAndSubmitTx(creationTx, account, {
-          reSign: true,
-          resolveOn,
-        })
-      }
-    )
-
-  await status('DID created on chain...')
-  return Did.DidResolver.resolveDoc(identifier)
-}
-
-async function getCTypes(didDoc, account, keypairs) {
-  let ctypeTx = []
-  const { api } =
-    await ChainHelpers.BlockchainApiConnection.getConnectionOrConnect()
-  const githubCType = CType.fromSchema({
-    $schema: 'http://kilt-protocol.org/draft-01/ctype#',
-    title: 'GitHub',
-    properties: {
-      Username: {
-        type: 'string',
-      },
-      'User ID': {
-        type: 'string',
-      },
-    },
-    type: 'object',
-  })
-
-  if (!(await githubCType.verifyStored())) {
-    ctypeTx.push(await githubCType.getStoreTx())
-  }
-
-  const discordCType = CType.fromSchema({
-    $schema: 'http://kilt-protocol.org/draft-01/ctype#',
-    title: 'Discord',
-    properties: {
-      Username: {
-        type: 'string',
-      },
-      Discriminator: {
-        type: 'string',
-      },
-      'User ID': {
-        type: 'string',
-      },
-    },
-    type: 'object',
-  })
-
-  if (!(await discordCType.verifyStored())) {
-    ctypeTx.push(await discordCType.getStoreTx())
-  }
-
-  const emailCType = CType.fromSchema({
-    $schema: 'http://kilt-protocol.org/draft-01/ctype#',
-    title: 'Email',
-    properties: {
-      Email: {
-        type: 'string',
-      },
-    },
-    type: 'object',
-  })
-
-  if (!(await emailCType.verifyStored())) {
-    ctypeTx.push(await emailCType.getStoreTx())
-  }
-
-  const twitchCType = CType.fromSchema({
-    $schema: 'http://kilt-protocol.org/draft-01/ctype#',
-    title: 'Twitch',
-    properties: {
-      Username: {
-        type: 'string',
-      },
-      'User ID': {
-        type: 'string',
-      },
-    },
-    type: 'object',
-  })
-  if (!(await twitchCType.verifyStored())) {
-    ctypeTx.push(await twitchCType.getStoreTx())
-  }
-
-  const twitterCType = CType.fromSchema({
-    $schema: 'http://kilt-protocol.org/draft-01/ctype#',
-    title: 'Twitter',
-    properties: {
-      Twitter: {
-        type: 'string',
-      },
-    },
-    type: 'object',
-  })
-
-  if (!(await twitterCType.verifyStored())) {
-    ctypeTx.push(await twitterCType.getStoreTx())
-  }
-
-  if (ctypeTx.length > 0) {
-    const batch = await new Did.DidBatchBuilder(api, didDoc.details)
-      .addMultipleExtrinsics(ctypeTx)
-      .consume(
-        {
-          async sign({ data, alg }) {
-            const { assertion } = keypairs
-            return {
-              data: assertion.sign(data, { withType: false }),
-              alg,
-            }
-          },
-        },
-        account.address
-      )
-
-    await BlockchainUtils.signAndSubmitTx(batch, account, {
-      reSign: true,
-      resolveOn,
-    })
-  }
-
-  return { githubCType, discordCType, emailCType, twitchCType, twitterCType }
-}
-
-export async function attestClaim(claims, fullDid, account, keypairs) {
-  const { api } =
-    await ChainHelpers.BlockchainApiConnection.getConnectionOrConnect()
-  const batchTx = await Promise.all(
-    claims.map(async ({ claim }) => {
-      const request = RequestForAttestation.fromClaim(claim)
-
-      await request.signWithDidKey(
-        {
-          async sign({ data, alg }) {
-            const { authentication } = keypairs
-            return {
-              data: authentication.sign(data, { withType: false }),
-              alg,
-            }
-          },
-        },
-        fullDid.details,
-        fullDid.details.authenticationKey.id
-      )
-
-      const attestation = Attestation.fromRequestAndDid(
-        request,
-        fullDid.details.did
-      )
-
-      const attested = Boolean(await Attestation.query(attestation.claimHash))
-      if (attested) return null
-
-      return attestation.getStoreTx()
-    })
-  )
-
-  if (batchTx.filter((val) => val !== true)) {
-    const batch = await new Did.DidBatchBuilder(api, fullDid.details)
-      .addMultipleExtrinsics(batchTx)
-      .consume(
-        {
-          async sign({ data, alg }) {
-            const { assertion } = keypairs
-            return {
-              data: assertion.sign(data, { withType: false }),
-              alg,
-            }
-          },
-        },
-        account.address
-      )
-
-    await BlockchainUtils.signAndSubmitTx(batch, account, {
-      reSign: true,
-      resolveOn,
-    })
-  }
-
-  return await Promise.all(
-    claims.map(async ({ ctype, claim }) => {
-      const request = RequestForAttestation.fromClaim(claim)
-
-      await request.signWithDidKey(
-        {
-          async sign({ data, alg }) {
-            const { authentication } = keypairs
-            return {
-              data: authentication.sign(data, { withType: false }),
-              alg,
-            }
-          },
-        },
-        fullDid.details,
-        fullDid.details.authenticationKey.id
-      )
-
-      return {
-        attester: 'PeregrineSelfAttestedSocialKYC',
-        cTypeTitle: ctype,
-        isDownloaded: true,
-        name: ctype,
-        ...request,
-      }
-    })
-  )
-}
 // to do: give each credential the correct name
 // Make the objects similar to the sporran object
 function saveAssets(credentials) {
@@ -368,7 +44,7 @@ export default async function () {
   )
 
   const { githubCType, discordCType, emailCType, twitchCType, twitterCType } =
-    await getCTypes(didDoc, account, keypairs)
+    await getAllSocialCTypes(didDoc, account, keypairs)
 
   await status('Generating claimer github claim content...')
   const githubClaimContents = await getClaimDetails(
@@ -429,7 +105,7 @@ export default async function () {
 
   await status('Self-attesting the claims...')
   const credentials = await attestClaim(claims, didDoc, account, keypairs)
-  
+
   await status('Generating claimer credentials assets...')
   saveAssets(credentials)
 
--- menus/setup-verifier.js
+++ menus/setup-verifier.js
@@ -1,48 +1,93 @@
-import { Attestation, BlockchainUtils, Credential, Claim, RequestForAttestation, CType, Utils, Did, KeyRelationship, init, Balance } from '@kiltprotocol/sdk-js'
-import { mnemonicGenerate, cryptoWaitReady, naclBoxPairFromSecret, sr25519PairFromSeed, mnemonicToMiniSecret, keyExtractPath, keyFromPath, blake2AsU8a } from '@polkadot/util-crypto'
-import { getMnemonic, useExisting, getNetwork, getOrigin, status } from "./_prompts.js"
+import {
+  Attestation,
+  Claim,
+  ChainHelpers,
+  CType,
+  Credential,
+  init,
+  RequestForAttestation,
+  Did,
+  BlockchainUtils,
+} from '@kiltprotocol/sdk-js'
+import {
+  mnemonicGenerate,
+  cryptoWaitReady,
+  encodeAddress,
+} from '@polkadot/util-crypto'
+import {
+  getMnemonic,
+  useExisting,
+  getNetwork,
+  getOrigin,
+  status,
+} from './_prompts.js'
+import {
+  getDidDoc,
+  getKeypairs,
+  loadAccount,
+  resolveOn,
+} from './utils/utils.js'
 import chalk from 'chalk'
 import fs from 'fs'
 import mainMenu from './main-menu.js'
 
 /**
  * setup verifier results in
- * - encryptionKey 
- * - didConfiguration 
- * - verifier mnemonic 
+ * - encryptionKey
+ * - didConfiguration
+ * - verifier mnemonic
  */
 
-export default async function({ returnAssets = false } = {}) {
+export default async function ({ returnAssets = false } = {}) {
   const network = await getNetwork()
-  const testnet = network.indexOf('peregrin') > -1 || network.indexOf('sporran') > -1
-  
-  const mnemonic = testnet ? 
-    await useExisting() ? 
-    await getMnemonic() : 
-    mnemonicGenerate() :
-    await getMnemonic()
+  const testnet =
+    network.indexOf('peregrin') > -1 || network.indexOf('sporran') > -1
+
+  const mnemonic = testnet
+    ? (await useExisting())
+      ? await getMnemonic()
+      : mnemonicGenerate()
+    : await getMnemonic()
 
   const origin = await getOrigin()
   await connect(network)
+  await ChainHelpers.BlockchainApiConnection.getConnectionOrConnect()
   const account = await loadAccount(mnemonic)
   const keypairs = await getKeypairs(account, mnemonic)
   const didDoc = await getDidDoc(account, keypairs, network)
-  const credential = await getDomainLinkCredential(keypairs, didDoc, origin)
-  const didConfig = await getDidConfiguration(credential)
-  const dotenv = await getEnvironmentVariables(network, mnemonic, account, didDoc, origin)
-  
-  if (returnAssets) return { 
-    network,
+  const credential = await getDomainLinkCredential(
+    keypairs,
+    didDoc,
     origin,
+    account
+  )
+  const didConfig = await getDidConfiguration(credential)
+  const dotenv = await getEnvironmentVariables(
+    network,
     mnemonic,
-    address: account.address,
-    didUri: didDoc.details.did,
-    dotenv, 
-    didConfig
-  }
+    account,
+    didDoc,
+    origin
+  )
+
+  if (returnAssets)
+    return {
+      network,
+      origin,
+      mnemonic,
+      address: account.address,
+      didUri: didDoc.details.did,
+      dotenv,
+      didConfig,
+    }
 
   await saveAssets(dotenv, didConfig)
-  await status(`Done! Assets saved to /verifier-assets\n${chalk.reset.gray('... press any key to return to main menu')}`, { keyPress: true })
+  await status(
+    `Done! Assets saved to /verifier-assets\n${chalk.reset.gray(
+      '... press any key to return to main menu'
+    )}`,
+    { keyPress: true }
+  )
   return mainMenu()
 }
 
@@ -52,79 +97,10 @@ async function connect(network) {
   await init({ address: network })
 }
 
-async function loadAccount(seed) {
-  await status('loading account...')
-  const signingKeyPairType = 'sr25519'
-  const keyring = new Utils.Keyring({
-    ss58Format: 38,
-    type: signingKeyPairType,
-  })
-
-  const mnemonic = seed || mnemonicGenerate()
-  const account = keyring.addFromUri(mnemonic);
-  return account
-}
-
-async function getKeypairs(account, mnemonic) {
-  await status('generating keypairs...')
-  const keypairs = {
-    authentication: account.derive('//did//0'),
-    assertion: account.derive('//did//assertion//0'),
-    keyAgreement: (function() {
-      const secretKeyPair = sr25519PairFromSeed(mnemonicToMiniSecret(mnemonic))
-      const { path } = keyExtractPath('//did//keyAgreement//0')
-      const { secretKey } = keyFromPath(secretKeyPair, path, 'sr25519')
-      const blake = blake2AsU8a(secretKey)
-      const boxPair = naclBoxPairFromSecret(blake)
-      return {
-        ...boxPair,
-        type: 'x25519',
-      }
-    }())
-  }
-
-  return {
-    ...keypairs,
-    relationships: {
-      [KeyRelationship.authentication]: keypairs.authentication,
-      [KeyRelationship.assertionMethod]: keypairs.assertion,
-      [KeyRelationship.keyAgreement]: keypairs.keyAgreement,
-    }
-  }
-}
-
-async function getDidDoc(account, keypairs, network) {
-  await status('checking for existing DID...')
-  const sign = async ({ data, alg }) => ({ data: keypairs.authentication.sign( data ), alg })
-  const { extrinsic, did } = await Did.DidUtils.writeDidFromPublicKeys({ sign }, account.address, keypairs.relationships)
-  let didDoc = await Did.DefaultResolver.resolveDoc(did)
-  if (didDoc) {
-    await status('DID loaded from chain...')
-    return didDoc
-  }
-
-  await status('checking balance...')
-  const testnet = network.indexOf('peregrine') > 0
-  let balance = parseInt((await Balance.getBalances(account.address)).free.toString())
-  if (balance < 3) {
-    const message = testnet 
-      ? chalk.red(`get testnet tokens to continue => https://faucet.peregrine.kilt.io/?${account.address}`)
-      : chalk.red(`balance too low... to continue send 3 or more KILT to: ${account.address}`)
-    
-    await status(message)
-    while(balance < 3) {
-      balance = parseInt((await Balance.getBalances(account.address)).free.toString())
-      await new Promise(r => setTimeout(r, 2000))
-    }
-  }
-
-  const resolveOn = BlockchainUtils.IS_FINALIZED
-  await BlockchainUtils.signAndSubmitTx(extrinsic, account, { resolveOn })
-  await status('DID created on chain...')
-  return await Did.DefaultResolver.resolveDoc(did)
-}
-
-async function getDomainLinkCredential(keypairs, didDoc, origin) {
+async function getDomainLinkCredential(keypairs, didDoc, origin, account) {
+  const fullDid = await Did.FullDidDetails.fromChainInfo(
+    encodeAddress(keypairs.authentication.publicKey, 38)
+  )
   await status('Creating Domain Linkage Credential...')
   const ctype = CType.fromSchema({
     $schema: 'http://kilt-protocol.org/draft-01/ctype#',
@@ -141,46 +117,62 @@ async function getDomainLinkCredential(keypairs, didDoc, origin) {
   })
 
   const claimContents = {
-    id: didDoc.details.did,
+    id: fullDid.did,
     origin: origin,
   }
 
   const claim = Claim.fromCTypeAndClaimContents(
     ctype,
     claimContents,
-    didDoc.details.did
+    fullDid.did
   )
 
-  const requestForAttestation = RequestForAttestation.fromClaim(claim)
-  const { signature, keyId } = await Did.DidUtils.signWithDid(
-    Utils.Crypto.coToUInt8(requestForAttestation.rootHash),
-    didDoc.details,
+  const request = RequestForAttestation.fromClaim(claim)
+
+  await request.signWithDidKey(
     {
       async sign({ data, alg }) {
-        const { assertion } = keypairs
+        const { authentication } = keypairs
         return {
-          data: assertion.sign(data, { withType: false }),
+          data: authentication.sign(data, { withType: false }),
           alg,
         }
       },
     },
-    didDoc.details.getKeyIds(KeyRelationship.assertionMethod)[0]
+    fullDid,
+    fullDid.authenticationKey.id
   )
 
-  const selfSignedRequest = await requestForAttestation.addSignature(
-    signature,
-    keyId
-  )
+  const attestation = Attestation.fromRequestAndDid(request, fullDid.did)
 
-  const attestation = Attestation.fromRequestAndDid(
-    selfSignedRequest,
-    didDoc.details.did
-  )
+  const attested = Boolean(await Attestation.query(attestation.claimHash))
+  if (attested) return null
 
-  return Credential.fromRequestAndAttestation(
-    selfSignedRequest,
-    attestation
-  )
+  await attestation
+    .getStoreTx()
+    .then((call) =>
+      fullDid.authorizeExtrinsic(
+        call,
+        {
+          async sign({ data, alg }) {
+            const { assertion } = keypairs
+            return {
+              data: assertion.sign(data, { withType: false }),
+              alg,
+            }
+          },
+        },
+        account.address
+      )
+    )
+    .then((tx) =>
+      BlockchainUtils.signAndSubmitTx(tx, account, {
+        reSign: true,
+        resolveOn,
+      })
+    )
+
+  return Credential.fromRequestAndAttestation(request, attestation)
 }
 
 async function getDidConfiguration(credential) {
@@ -194,7 +186,7 @@ async function getDidConfiguration(credential) {
 
   const issuanceDate = new Date().toISOString()
   const expirationDate = new Date(
-    Date.now() + 1000 * 60 * 60 * 24 * 365 * 5,
+    Date.now() + 1000 * 60 * 60 * 24 * 365 * 5
   ).toISOString() // 5 years
 
   const { claimerSignature } = credential.request
@@ -207,29 +199,37 @@ async function getDidConfiguration(credential) {
     signature: claimerSignature.signature,
     challenge: claimerSignature.challenge,
   }
-  
+
   return {
-    "@context": "https://identity.foundation/.well-known/did-configuration/v1",
-    linked_dids: [{
-      '@context': [
-        'https://www.w3.org/2018/credentials/v1',
-        'https://identity.foundation/.well-known/did-configuration/v1',
-      ],
-      issuer,
-      issuanceDate,
-      expirationDate,
-      type: [
-        'VerifiableCredential',
-        'DomainLinkageCredential',
-        'KiltCredential2020',
-      ],
-      credentialSubject,
-      proof,
-    }]
+    '@context': 'https://identity.foundation/.well-known/did-configuration/v1',
+    linked_dids: [
+      {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          'https://identity.foundation/.well-known/did-configuration/v1',
+        ],
+        issuer,
+        issuanceDate,
+        expirationDate,
+        type: [
+          'VerifiableCredential',
+          'DomainLinkageCredential',
+          'KiltCredential2020',
+        ],
+        credentialSubject,
+        proof,
+      },
+    ],
   }
 }
 
-async function getEnvironmentVariables(network, mnemonic, account, didDoc, origin) {
+async function getEnvironmentVariables(
+  network,
+  mnemonic,
+  account,
+  didDoc,
+  origin
+) {
   await status('Building environment variables...')
   let dotenv = ''
   dotenv += `ORIGIN=${origin}\n`
@@ -246,6 +246,10 @@ async function saveAssets(dotenv, didConfig) {
   fs.rmSync(directory, { recursive: true, force: true })
   fs.mkdirSync(directory)
   fs.writeFileSync(`${directory}/.env`, dotenv, 'utf-8')
-  fs.writeFileSync(`${directory}/didConfiguration.json`, JSON.stringify(didConfig, null, 2), 'utf-8')
+  fs.writeFileSync(
+    `${directory}/didConfiguration.json`,
+    JSON.stringify(didConfig, null, 2),
+    'utf-8'
+  )
   return true
 }
