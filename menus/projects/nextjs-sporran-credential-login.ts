dd374fa2085304315a237985314549612f60720d
dudleyneedham <dudleyneedham1993@gmail.com>
2022-08-11 15:01:06 +0200

    feat: initial typescript commit

---
+++ menus/projects/nextjs-sporran-credential-login.ts
@@ -0,0 +1,45 @@
+import {
+  status,
+  getJWTExpiry,
+  getJWTRenewal,
+  createTestCredentials,
+} from '../_prompts.js'
+import setupVerifier from '../setup-verifier.js'
+import { randomAsHex } from '@polkadot/util-crypto'
+import setupClaimer from '../setup-claimer.js'
+import fs from 'fs-extra'
+import exitCli from '../exit-cli.js'
+
+export default async function (dappName) {
+  const jwtSecret = randomAsHex(16)
+  const jwtExpiry = await getJWTExpiry()
+  const renewJwt = await getJWTRenewal()
+  const { origin, network, mnemonic, address, didUri } = await setupVerifier({
+    returnAssets: true,
+  })
+
+  let dotenv = ''
+  dotenv += `DAPP_NAME="${dappName}"\n`
+  dotenv += `ORIGIN=${origin}\n`
+  dotenv += `WSS_ADDRESS=${network}\n`
+  dotenv += `VERIFIER_MNEMONIC="${mnemonic}"\n`
+  dotenv += `VERIFIER_ADDRESS=${address}\n`
+  dotenv += `VERIFIER_DID_URI=${didUri}\n`
+  dotenv += `JWT_SECRET=${jwtSecret}\n`
+  dotenv += `JWT_EXPIRY=${jwtExpiry}\n`
+  dotenv += `JWT_RENEW=${renewJwt ? 'true' : 'false'}`
+
+  status('creating files...')
+  fs.writeFileSync(`${process.cwd()}/${dappName}/.env`, dotenv)
+
+  if (!network.startsWith('wss://spiritnet')) {
+    const needClaimer = await createTestCredentials()
+    if (needClaimer) await setupClaimer()
+  }
+
+  await status(
+    `all done! to run the project:\nmove to '${dappName}' directory\nnpm install\nnpm run dev`
+  )
+
+  return exitCli()
+}
---
+++ menus/setup-verifier.ts
@@ -0,0 +1,96 @@
+import { ChainHelpers, init } from '@kiltprotocol/sdk-js'
+import { mnemonicGenerate, cryptoWaitReady } from '@polkadot/util-crypto'
+import {
+  getMnemonic,
+  useExisting,
+  getNetwork,
+  getOrigin,
+  status,
+} from './_prompts.js'
+import { getDidDoc, getKeypairs, loadAccount } from './utils/utils.js'
+import chalk from 'chalk'
+import fs from 'fs'
+import mainMenu from './main-menu.js'
+
+/**
+ * setup verifier results in
+ * - encryptionKey
+ * - verifier mnemonic
+ */
+
+export default async function ({ returnAssets = false } = {}) {
+  const network = await getNetwork()
+  const testnet =
+    network.indexOf('peregrin') > -1 || network.indexOf('sporran') > -1
+
+  const mnemonic = testnet
+    ? (await useExisting())
+      ? await getMnemonic()
+      : mnemonicGenerate()
+    : await getMnemonic()
+
+  const origin = await getOrigin()
+  await connect(network)
+  await ChainHelpers.BlockchainApiConnection.getConnectionOrConnect()
+  const account = await loadAccount(mnemonic)
+  const keypairs = await getKeypairs(account, mnemonic)
+  const didDoc = await getDidDoc(account, keypairs, network)
+  const dotenv = await getEnvironmentVariables(
+    network,
+    mnemonic,
+    account,
+    didDoc,
+    origin
+  )
+
+  if (returnAssets)
+    return {
+      network,
+      origin,
+      mnemonic,
+      address: account.address,
+      didUri: didDoc.details.uri,
+      dotenv,
+    }
+
+  await saveAssets(dotenv)
+  await status(
+    `Done! Assets saved to /verifier-assets\n${chalk.reset.gray(
+      '... press any key to return to main menu'
+    )}`,
+    { keyPress: true }
+  )
+  return mainMenu()
+}
+
+async function connect(network) {
+  await status('connecting to network...')
+  await cryptoWaitReady()
+  await init({ address: network })
+}
+
+async function getEnvironmentVariables(
+  network,
+  mnemonic,
+  account,
+  didDoc,
+  origin
+) {
+  await status('Building environment variables...')
+  let dotenv = ''
+  dotenv += `ORIGIN=${origin}\n`
+  dotenv += `WSS_ADDRESS=${network}\n`
+  dotenv += `VERIFIER_MNEMONIC=${mnemonic}\n`
+  dotenv += `VERIFIER_ADDRESS=${account.address}\n`
+  dotenv += `VERIFIER_DID_URI=${didDoc.details.uri}`
+  return dotenv
+}
+
+async function saveAssets(dotenv) {
+  await status('Generating Verifier assets...')
+  const directory = `${process.cwd()}/verifier-assets`
+  fs.rmSync(directory, { recursive: true, force: true })
+  fs.mkdirSync(directory)
+  fs.writeFileSync(`${directory}/.env`, dotenv, 'utf-8')
+  return true
+}
