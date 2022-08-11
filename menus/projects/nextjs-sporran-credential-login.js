3b5fe4b5a3bd55cdca4939933501e1ebfa5df525
dudleyneedham <dudleyneedham1993@gmail.com>
2022-08-11 14:53:52 +0200

    feat: removal of did config

--- menus/projects/nextjs-sporran-credential-login.js
+++ menus/projects/nextjs-sporran-credential-login.js
@@ -1,22 +1,22 @@
-import { status, getJWTExpiry, getJWTRenewal, createTestCredentials } from "../_prompts.js"
-import setupVerifier from "../setup-verifier.js"
-import { randomAsHex } from "@polkadot/util-crypto"
-import setupClaimer from "../setup-claimer.js"
+import {
+  status,
+  getJWTExpiry,
+  getJWTRenewal,
+  createTestCredentials,
+} from '../_prompts.js'
+import setupVerifier from '../setup-verifier.js'
+import { randomAsHex } from '@polkadot/util-crypto'
+import setupClaimer from '../setup-claimer.js'
 import fs from 'fs-extra'
-import exitCli from "../exit-cli.js"
+import exitCli from '../exit-cli.js'
 
 export default async function (dappName) {
   const jwtSecret = randomAsHex(16)
   const jwtExpiry = await getJWTExpiry()
   const renewJwt = await getJWTRenewal()
-  const { 
-    origin,
-    network,
-    mnemonic,
-    address,
-    didUri,
-    didConfig, 
-  } = await setupVerifier({ returnAssets: true })
+  const { origin, network, mnemonic, address, didUri } = await setupVerifier({
+    returnAssets: true,
+  })
 
   let dotenv = ''
   dotenv += `DAPP_NAME="${dappName}"\n`
@@ -31,14 +31,15 @@ export default async function (dappName) {
 
   status('creating files...')
   fs.writeFileSync(`${process.cwd()}/${dappName}/.env`, dotenv)
-  fs.writeFileSync(`${process.cwd()}/${dappName}/public/didConfiguration.json`, JSON.stringify(didConfig, null, 2))
 
   if (!network.startsWith('wss://spiritnet')) {
     const needClaimer = await createTestCredentials()
     if (needClaimer) await setupClaimer()
   }
-  
-  await status(`all done! to run the project:\nmove to '${dappName}' directory\nnpm install\nnpm run dev`)
+
+  await status(
+    `all done! to run the project:\nmove to '${dappName}' directory\nnpm install\nnpm run dev`
+  )
 
   return exitCli()
 }
