2d5d35c47b93e22d806b575585cda83fa939df6b
kilted-andres <andres@trusted-entity.io>
2022-11-16 16:59:00 +0100

    latest

---
+++ Andres_Jungfernheit/pages/api/user.js
@@ -0,0 +1,34 @@
+import { clearCookie, setCookie, createJWT, getCookieData } from '../../utilities/auth';
+import { ConfigService, Did } from '@kiltprotocol/sdk-js'
+
+
+
+export default async function handler(req, res) {
+  // load and parse the cookie
+  const cookie = req.headers.cookie || ''
+
+  // get the user from cookie
+  const user = getCookieData({ name: 'token', cookie }) // did-uri from the extension
+
+  if (!user) {
+    // if null ensure cookie is cleared & 401
+    clearCookie(res, { name: 'token'})
+    res.status(401).send('')
+  } else {
+    // if user and renew reset the token
+    const renew = process.env.JWT_RENEW
+    const api = ConfigService.get('api');
+    if (renew) {
+      const newToken = createJWT(user)
+      setCookie(res, { name: 'token', data: newToken })
+    }
+
+    const web3Name = await api.query.web3Names.names(user);
+
+    //console.log(await api.query.did.did())
+
+    //.queryWeb3NameForDid(user)
+    // send user and 200
+    res.status(200).send(web3Name ? web3Name : user)
+  }
+}
